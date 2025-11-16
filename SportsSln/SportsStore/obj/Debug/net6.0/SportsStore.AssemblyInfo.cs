10cc22f5f46a30bfae97e0f0a898411102184f1b
albainox <albertbrzakala@tlen.pl>
2025-11-16 10:14:15 +0100

    seed data and test

--- SportsSln/SportsStore/obj/Debug/net6.0/SportsStore.AssemblyInfo.cs
+++ SportsSln/SportsStore/obj/Debug/net6.0/SportsStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ae04247e7fbf13e2a5588374b9c0f42976d941")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
